4be77d9343634ec5ec9916bbe6bd608e2223f206
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2018-11-02 16:16:56 -0400

    Updates models for use with dnaorg_scripts 0.37 [blastx protein validation]

--- ebolavirus-builds/ebolavirus-builds.dnaorg_classify.cmd
+++ ebolavirus-builds/ebolavirus-builds.dnaorg_classify.cmd
@@ -1,25 +1,25 @@
 mkdir ebolavirus-builds
 cp ebolavirus_refseq_list.txt ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.list
 cat ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.list | epost -db nuccore -format acc | efetch -format fasta > ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-seqstat --dna -a ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa | grep ^= | awk '{ printf("%s %s\n", $2, $3); }' > ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa.list.tmp
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-seqstat --dna -a ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa | grep ^= | awk '{ printf("%s %s\n", $2, $3); }' > ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa.list.tmp
 rm ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa.list.tmp
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch --index ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa > /dev/null
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa NC_006432.1 | /panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_006432 ebolavirus-builds/ebolavirus-builds.dnaorg_classify.0.hmm - > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch --index ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa NC_006432.1 | /panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_006432 ebolavirus-builds/ebolavirus-builds.dnaorg_classify.0.hmm - > /dev/null
 cat ebolavirus-builds/ebolavirus-builds.dnaorg_classify.0.hmm >> ebolavirus-builds/ebolavirus-builds.dnaorg_classify.hmm
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa NC_014373.1 | /panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_014373 ebolavirus-builds/ebolavirus-builds.dnaorg_classify.1.hmm - > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa NC_014373.1 | /panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_014373 ebolavirus-builds/ebolavirus-builds.dnaorg_classify.1.hmm - > /dev/null
 cat ebolavirus-builds/ebolavirus-builds.dnaorg_classify.1.hmm >> ebolavirus-builds/ebolavirus-builds.dnaorg_classify.hmm
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa NC_002549.1 | /panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_002549 ebolavirus-builds/ebolavirus-builds.dnaorg_classify.2.hmm - > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa NC_002549.1 | /panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_002549 ebolavirus-builds/ebolavirus-builds.dnaorg_classify.2.hmm - > /dev/null
 cat ebolavirus-builds/ebolavirus-builds.dnaorg_classify.2.hmm >> ebolavirus-builds/ebolavirus-builds.dnaorg_classify.hmm
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa NC_014372.1 | /panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_014372 ebolavirus-builds/ebolavirus-builds.dnaorg_classify.3.hmm - > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa NC_014372.1 | /panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_014372 ebolavirus-builds/ebolavirus-builds.dnaorg_classify.3.hmm - > /dev/null
 cat ebolavirus-builds/ebolavirus-builds.dnaorg_classify.3.hmm >> ebolavirus-builds/ebolavirus-builds.dnaorg_classify.hmm
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa NC_004161.1 | /panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_004161 ebolavirus-builds/ebolavirus-builds.dnaorg_classify.4.hmm - > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch ebolavirus-builds/ebolavirus-builds.dnaorg_classify.ref.fa NC_004161.1 | /panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_004161 ebolavirus-builds/ebolavirus-builds.dnaorg_classify.4.hmm - > /dev/null
 cat ebolavirus-builds/ebolavirus-builds.dnaorg_classify.4.hmm >> ebolavirus-builds/ebolavirus-builds.dnaorg_classify.hmm
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmpress ebolavirus-builds/ebolavirus-builds.dnaorg_classify.hmm > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmpress ebolavirus-builds/ebolavirus-builds.dnaorg_classify.hmm > /dev/null
 rm ebolavirus-builds/ebolavirus-builds.dnaorg_classify.0.hmm
 rm ebolavirus-builds/ebolavirus-builds.dnaorg_classify.1.hmm
 rm ebolavirus-builds/ebolavirus-builds.dnaorg_classify.2.hmm
 rm ebolavirus-builds/ebolavirus-builds.dnaorg_classify.3.hmm
 rm ebolavirus-builds/ebolavirus-builds.dnaorg_classify.4.hmm
-# Tue Aug 21 10:05:52 EDT 2018
-# Linux cbbdev21 3.10.0-862.9.1.el7.x86_64 #1 SMP Mon Jul 16 16:29:36 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux
+# Mon Oct 29 11:43:52 EDT 2018
+# Linux cbbdev21 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux
 # DNAORG-SUCCESS
--- norovirus-builds/norovirus-builds.dnaorg_classify.cmd
+++ norovirus-builds/norovirus-builds.dnaorg_classify.cmd
@@ -1,28 +1,28 @@
 mkdir norovirus-builds
 cp norovirus_refseq_list.txt norovirus-builds/norovirus-builds.dnaorg_classify.ref.list
 cat norovirus-builds/norovirus-builds.dnaorg_classify.ref.list | epost -db nuccore -format acc | efetch -format fasta > norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-seqstat --dna -a norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa | grep ^= | awk '{ printf("%s %s\n", $2, $3); }' > norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa.list.tmp
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-seqstat --dna -a norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa | grep ^= | awk '{ printf("%s %s\n", $2, $3); }' > norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa.list.tmp
 rm norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa.list.tmp
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch --index norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa > /dev/null
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa NC_001959.2 | /panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_001959 norovirus-builds/norovirus-builds.dnaorg_classify.0.hmm - > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch --index norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa NC_001959.2 | /panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_001959 norovirus-builds/norovirus-builds.dnaorg_classify.0.hmm - > /dev/null
 cat norovirus-builds/norovirus-builds.dnaorg_classify.0.hmm >> norovirus-builds/norovirus-builds.dnaorg_classify.hmm
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa NC_008311.1 | /panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_008311 norovirus-builds/norovirus-builds.dnaorg_classify.1.hmm - > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa NC_008311.1 | /panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_008311 norovirus-builds/norovirus-builds.dnaorg_classify.1.hmm - > /dev/null
 cat norovirus-builds/norovirus-builds.dnaorg_classify.1.hmm >> norovirus-builds/norovirus-builds.dnaorg_classify.hmm
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa NC_029645.1 | /panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_029645 norovirus-builds/norovirus-builds.dnaorg_classify.2.hmm - > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa NC_029645.1 | /panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_029645 norovirus-builds/norovirus-builds.dnaorg_classify.2.hmm - > /dev/null
 cat norovirus-builds/norovirus-builds.dnaorg_classify.2.hmm >> norovirus-builds/norovirus-builds.dnaorg_classify.hmm
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa NC_029646.1 | /panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_029646 norovirus-builds/norovirus-builds.dnaorg_classify.3.hmm - > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa NC_029646.1 | /panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_029646 norovirus-builds/norovirus-builds.dnaorg_classify.3.hmm - > /dev/null
 cat norovirus-builds/norovirus-builds.dnaorg_classify.3.hmm >> norovirus-builds/norovirus-builds.dnaorg_classify.hmm
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa NC_029647.1 | /panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_029647 norovirus-builds/norovirus-builds.dnaorg_classify.4.hmm - > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa NC_029647.1 | /panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_029647 norovirus-builds/norovirus-builds.dnaorg_classify.4.hmm - > /dev/null
 cat norovirus-builds/norovirus-builds.dnaorg_classify.4.hmm >> norovirus-builds/norovirus-builds.dnaorg_classify.hmm
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/infernal-1.1.2/easel/miniapps/esl-sfetch norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa NC_031324.1 | /panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_031324 norovirus-builds/norovirus-builds.dnaorg_classify.5.hmm - > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/infernal-1.1.2/easel/miniapps/esl-sfetch norovirus-builds/norovirus-builds.dnaorg_classify.ref.fa NC_031324.1 | /panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmbuild --informat afa -n NC_031324 norovirus-builds/norovirus-builds.dnaorg_classify.5.hmm - > /dev/null
 cat norovirus-builds/norovirus-builds.dnaorg_classify.5.hmm >> norovirus-builds/norovirus-builds.dnaorg_classify.hmm
-/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/hmmer-3.1b2/src/hmmpress norovirus-builds/norovirus-builds.dnaorg_classify.hmm > /dev/null
+/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/hmmer-3.1b2/src/hmmpress norovirus-builds/norovirus-builds.dnaorg_classify.hmm > /dev/null
 rm norovirus-builds/norovirus-builds.dnaorg_classify.0.hmm
 rm norovirus-builds/norovirus-builds.dnaorg_classify.1.hmm
 rm norovirus-builds/norovirus-builds.dnaorg_classify.2.hmm
 rm norovirus-builds/norovirus-builds.dnaorg_classify.3.hmm
 rm norovirus-builds/norovirus-builds.dnaorg_classify.4.hmm
 rm norovirus-builds/norovirus-builds.dnaorg_classify.5.hmm
-# Tue Aug 21 09:58:14 EDT 2018
-# Linux cbbdev21 3.10.0-862.9.1.el7.x86_64 #1 SMP Mon Jul 16 16:29:36 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux
+# Wed Oct 24 14:44:12 EDT 2018
+# Linux cbbdev21 3.10.0-862.11.6.el7.x86_64 #1 SMP Tue Aug 14 21:49:04 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux
 # DNAORG-SUCCESS
