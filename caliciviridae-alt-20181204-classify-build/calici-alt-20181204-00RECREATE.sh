e628645708f5b175827a054150c0ae981f766711
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2018-12-04 09:38:18 -0500

    Adds alternate caliciviridae HMM dir and files, for testing the replacement of Norovirus NC_031324 with NC_0398897

---
+++ caliciviridae-alt-20181204-classify-build/calici-alt-20181204-00RECREATE.sh
@@ -0,0 +1,3 @@
+# making an alternative caliciviridae HMM file to test the idea of replacing
+# the norovirus NC_031324 with NC_039897.
+dnaorg_classify.pl --dirout caliciviridae-alt-20181204-classify-build --onlybuild caliciviridae_alt_20181204_refseq_list.txt 
